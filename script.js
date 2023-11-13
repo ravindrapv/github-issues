d1b3d5a85c843f4b68fcebd954a9f4b0fa49438d
ravindrapv <ravindra.ravi4849@gmail.com>
2023-11-13 14:26:14 +0530

    issues-delete-pending

---
+++ script.js
@@ -0,0 +1,110 @@
+const owner = "ravindrapv";
+const repo = "update-code-of-geetha";
+const token =
+  "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";
+
+async function showIssues() {
+  const response = await fetch(
+    `https://api.github.com/repos/${owner}/${repo}/issues`
+  );
+  const issues = await response.json();
+
+  const issuesList = document.getElementById("issues-list");
+  issuesList.innerHTML = "";
+
+  issues.forEach((issue) => {
+    const li = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.value = issue.number;
+    li.appendChild(checkbox);
+
+    const titleSpan = document.createElement("span");
+    titleSpan.textContent = issue.title;
+    li.appendChild(titleSpan);
+
+    issuesList.appendChild(li);
+  });
+}
+
+async function createAndShowIssue() {
+  const issueTitleInput = document.getElementById("issue-title");
+  const issueBodyInput = document.getElementById("issue-body");
+
+  const issueData = {
+    title: issueTitleInput.value,
+    body: issueBodyInput.value,
+  };
+
+  const response = await fetch(
+    `https://api.github.com/repos/${owner}/${repo}/issues`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(issueData),
+    }
+  );
+
+  const newIssue = await response.json();
+  console.log("New issue created:", newIssue);
+
+  showIssues();
+}
+
+async function updateSelectedIssues() {
+  const checkboxes = document.querySelectorAll(
+    '#issues-list input[type="checkbox"]:checked'
+  );
+
+  for (const checkbox of checkboxes) {
+    const issueNumber = checkbox.value;
+
+    const issueTitleInput = document.getElementById("issue-title");
+    const issueBodyInput = document.getElementById("issue-body");
+
+    const updateData = {
+      title: issueTitleInput.value,
+      body: issueBodyInput.value,
+    };
+
+    await fetch(
+      `https://api.github.com/repos/${owner}/${repo}/issues/${issueNumber}`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updateData),
+      }
+    );
+  }
+
+  showIssues();
+}
+
+async function deleteSelectedIssues() {
+  const checkboxes = document.querySelectorAll(
+    '#issues-list input[type="checkbox"]:checked'
+  );
+
+  for (const checkbox of checkboxes) {
+    const issueNumber = checkbox.value;
+
+    await fetch(
+      `https://api.github.com/repos/${owner}/${repo}/issues/${issueNumber}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+  }
+
+  showIssues();
+}
+showIssues();
