63d6895ce93be060b5d124004b8da377e13083ed
ravindrapv <ravindra.ravi4849@gmail.com>
2023-11-18 12:36:06 +0530

    update

---
+++ raedme.md
@@ -0,0 +1,22 @@
+# GitHub Issue Manager
+
+This project is a simple web-based GitHub issue manager that allows users to view, create, update, and delete issues in a GitHub repository.
+
+## Getting Started
+
+### Prerequisites
+
+Before you begin, make sure you have the following installed:
+
+- Web browser
+- Internet connection
+
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/ravindrapv/update-code-of-geetha.git
+
+2. start live server
--- script.js
+++ script.js
@@ -1,110 +1,164 @@
 const owner = "ravindrapv";
 const repo = "update-code-of-geetha";
 const token =
-  "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";
+  "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";
+async function fetchData(url, options = {}) {
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error:", error.message);
+    showFlashMessage("An error occurred. Please try again.", "error");
+    throw error;
+  }
+}
+
 async function showIssues() {
-  const response = await fetch(
-    `https://api.github.com/repos/${owner}/${repo}/issues`
-  );
-  const issues = await response.json();
+  try {
+    const issues = await fetchData(
+      `https://api.github.com/repos/${owner}/${repo}/issues`
+    );
 
-  const issuesList = document.getElementById("issues-list");
-  issuesList.innerHTML = "";
+    const issuesList = document.getElementById("issues-list");
+    issuesList.innerHTML = "";
 
-  issues.forEach((issue) => {
-    const li = document.createElement("li");
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.value = issue.number;
-    li.appendChild(checkbox);
+    issues.forEach((issue) => {
+      const li = document.createElement("li");
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.value = issue.number;
+      li.appendChild(checkbox);
 
-    const titleSpan = document.createElement("span");
-    titleSpan.textContent = issue.title;
-    li.appendChild(titleSpan);
+      const titleSpan = document.createElement("span");
+      titleSpan.textContent = issue.title;
+      li.appendChild(titleSpan);
 
-    issuesList.appendChild(li);
-  });
+      issuesList.appendChild(li);
+    });
+  } catch (error) {
+    console.error("Error fetching issues:", error.message);
+  }
 }
 
 async function createAndShowIssue() {
-  const issueTitleInput = document.getElementById("issue-title");
-  const issueBodyInput = document.getElementById("issue-body");
-
-  const issueData = {
-    title: issueTitleInput.value,
-    body: issueBodyInput.value,
-  };
-
-  const response = await fetch(
-    `https://api.github.com/repos/${owner}/${repo}/issues`,
-    {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(issueData),
-    }
-  );
-
-  const newIssue = await response.json();
-  console.log("New issue created:", newIssue);
-
-  showIssues();
-}
-
-async function updateSelectedIssues() {
-  const checkboxes = document.querySelectorAll(
-    '#issues-list input[type="checkbox"]:checked'
-  );
-
-  for (const checkbox of checkboxes) {
-    const issueNumber = checkbox.value;
-
+  try {
     const issueTitleInput = document.getElementById("issue-title");
     const issueBodyInput = document.getElementById("issue-body");
 
-    const updateData = {
+    const issueData = {
       title: issueTitleInput.value,
       body: issueBodyInput.value,
     };
 
-    await fetch(
-      `https://api.github.com/repos/${owner}/${repo}/issues/${issueNumber}`,
+    const newIssue = await fetchData(
+      `https://api.github.com/repos/${owner}/${repo}/issues`,
       {
-        method: "PATCH",
+        method: "POST",
         headers: {
           Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(updateData),
+        body: JSON.stringify(issueData),
       }
     );
-  }
 
-  showIssues();
+    console.log("New issue created:", newIssue);
+    showIssues();
+    showFlashMessage("Issue created successfully!");
+  } catch (error) {
+    console.error("Error creating issue:", error.message);
+  }
+}
+
+async function updateSelectedIssues() {
+  try {
+    const checkboxes = document.querySelectorAll(
+      '#issues-list input[type="checkbox"]:checked'
+    );
+
+    for (const checkbox of checkboxes) {
+      const issueNumber = checkbox.value;
+
+      const issueTitleInput = document.getElementById("issue-title");
+      const issueBodyInput = document.getElementById("issue-body");
+
+      const updateData = {
+        title: issueTitleInput.value,
+        body: issueBodyInput.value,
+      };
+
+      await fetchData(
+        `https://api.github.com/repos/${owner}/${repo}/issues/${issueNumber}`,
+        {
+          method: "PATCH",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updateData),
+        }
+      );
+    }
+
+    showIssues();
+    location.reload();
+    showFlashMessage("Issues updated successfully!");
+  } catch (error) {
+    console.error("Error updating issues:", error.message);
+  }
 }
 
 async function deleteSelectedIssues() {
-  const checkboxes = document.querySelectorAll(
-    '#issues-list input[type="checkbox"]:checked'
-  );
-
-  for (const checkbox of checkboxes) {
-    const issueNumber = checkbox.value;
-
-    await fetch(
-      `https://api.github.com/repos/${owner}/${repo}/issues/${issueNumber}`,
-      {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
+  try {
+    const checkboxes = document.querySelectorAll(
+      '#issues-list input[type="checkbox"]:checked'
     );
-  }
 
-  showIssues();
+    for (const checkbox of checkboxes) {
+      const issueNumber = checkbox.value;
+
+      await fetchData(
+        `https://api.github.com/repos/${owner}/${repo}/issues/${issueNumber}`,
+        {
+          method: "PATCH",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            state: "closed",
+          }),
+        }
+      );
+    }
+
+    showIssues();
+    showFlashMessage("Issue(s) closed successfully!");
+  } catch (error) {
+    console.error("Error deleting issues:", error.message);
+  }
+}
+
+function showFlashMessage(message, messageType = "success") {
+  const flashMessage = document.getElementById("flash-message");
+  flashMessage.textContent = message;
+  flashMessage.style.display = "block";
+  flashMessage.style.padding = "10px";
+  flashMessage.style.margin = "10px 0";
+  flashMessage.style.borderRadius = "5px";
+  if (messageType === "success") {
+    flashMessage.style.backgroundColor = "#4CAF50";
+    flashMessage.style.color = "#fff";
+  } else if (messageType === "error") {
+    flashMessage.style.backgroundColor = "#ff5252";
+    flashMessage.style.color = "#fff";
+  }
+  setTimeout(() => {
+    flashMessage.style.display = "none";
+  }, 5000);
 }
 showIssues();
 // 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;
